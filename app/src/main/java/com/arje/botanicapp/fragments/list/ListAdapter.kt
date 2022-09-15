cf68e1f8a6f112a281656fd1446c1ab0447deb24
Arek Jedrzejewski <arkadiusz_jedrzejewskI@epam.com>
2022-09-15 17:08:12 +0200

    data displaying

---
+++ app/src/main/java/com/arje/botanicapp/fragments/list/ListAdapter.kt
@@ -0,0 +1,38 @@
+package com.arje.botanicapp.fragments.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.arje.botanicapp.R
+import com.arje.botanicapp.data.Plant
+import kotlinx.android.synthetic.main.custom_row.view.*
+
+class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    private var plantList = emptyList<Plant>()
+
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = plantList[position]
+        holder.itemView.txt_plantName.text = currentItem.name
+    }
+
+    override fun getItemCount(): Int {
+        return plantList.size
+    }
+
+    fun setData(plants: List<Plant>) {
+        this.plantList = plants
+//        this.plantList = plants.sortedWith(compareBy(String.CASE_INSENSITIVE_ORDER) { it.name })
+        notifyDataSetChanged()
+    }
+
+}
--- app/src/main/java/com/arje/botanicapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/arje/botanicapp/fragments/list/ListFragment.kt
@@ -5,18 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.arje.botanicapp.R
+import com.arje.botanicapp.data.PlantViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 class ListFragment : Fragment() {
+
+    private lateinit var myPlantViewModel: PlantViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        val adapter = ListAdapter()
+        val recyclerview = view.recyclerview
+        recyclerview.adapter = adapter
+        recyclerview.layoutManager = LinearLayoutManager(requireContext())
+
+        myPlantViewModel = ViewModelProvider(this)[PlantViewModel::class.java]
+        myPlantViewModel.readAllData.observe(viewLifecycleOwner) { plant ->
+            adapter.setData(plant)
+        }
 
         view.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
