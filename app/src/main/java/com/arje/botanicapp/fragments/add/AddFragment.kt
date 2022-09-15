5b1f1ca00dbb654a866e8acb4d007e89200dba9c
Arek Jedrzejewski <arkadiusz_jedrzejewskI@epam.com>
2022-09-15 15:17:36 +0200

    data insertion

--- app/src/main/java/com/arje/botanicapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/arje/botanicapp/fragments/add/AddFragment.kt
@@ -5,16 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.arje.botanicapp.R
+import com.arje.botanicapp.data.Plant
+import com.arje.botanicapp.data.PlantViewModel
+import kotlinx.android.synthetic.main.fragment_add.*
+import kotlinx.android.synthetic.main.fragment_add.view.*
 
 class AddFragment : Fragment() {
+
+    private lateinit var myPlantViewModel: PlantViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        myPlantViewModel = ViewModelProvider(this).get(PlantViewModel::class.java)
+
+        view.btn_add.setOnClickListener {
+            insertNewPlant()
+        }
+
+        return view
+    }
+
+    private fun insertNewPlant() {
+        val plantName = et_plantName.text.toString()
+
+        if (!validate(plantName)) {
+            Toast.makeText(requireContext(), "Plant name can't be blank!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val plant = Plant(0, plantName)
+        myPlantViewModel.addPlant(plant)
+        Toast.makeText(requireContext(), "Successfully added plant!", Toast.LENGTH_LONG).show()
+        findNavController().navigate(R.id.action_addFragment_to_listFragment)
+    }
+
+    private fun validate(plantName: String): Boolean {
+        return plantName.isNotBlank()
     }
 
 }
